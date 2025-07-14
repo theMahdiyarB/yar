58e28b4a65b67ae4d5dabbf32216db68a14fefb3
Mahdiyar Baladzadeh <33492952+theMahdiyarB@users.noreply.github.com>
2025-07-14 20:20:15 +0330

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = 'https://lebeltkaxxbpqubobqmz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlYmVsdGtheHhicHF1Ym9icW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0MTkzMTksImV4cCI6MjA2Nzk5NTMxOX0.n25fjWAr7Npi5RdkfHfW_uQK5xU2kgqZj30ets5VjA8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 /**
  * Creates a Supabase client for browser-side usage.
